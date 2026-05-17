25534fafa11677bdae39439d98f4ee6494f5fce9
ThiagoFrederico <thiagofrederico3@gmail.com>
2026-05-17 03:20:16 -0300

    remove key

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
     bucket         = "tf-state-ai-architecture-analyzer"
-    key            = "v1/gateway/dev/terraform.tfstate"
     region         = "us-east-2"
     encrypt        = true
   }
